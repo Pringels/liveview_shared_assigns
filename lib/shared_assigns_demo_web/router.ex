35d527642504285d163ac9a1a98d303dc195f85c
Fly Dev <dev@fly.io>
2025-07-04 05:19:13 +0000

    replace home route with SharedAssigns demo route

--- lib/shared_assigns_demo_web/router.ex
+++ lib/shared_assigns_demo_web/router.ex
@@ -17,7 +17,7 @@ defmodule SharedAssignsDemoWeb.Router do
   scope "/", SharedAssignsDemoWeb do
     pipe_through :browser
 
-    get "/", PageController, :home
+    live "/", PageLive, :index
   end
 
   # Other scopes may use custom stacks.
