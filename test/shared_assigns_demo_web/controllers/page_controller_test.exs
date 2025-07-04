a2f46cf6883445d60260a55e5cf2e048fd5c1539
Fly Dev <dev@fly.io>
2025-07-04 12:39:26 +0000

    fix PageControllerTest to match SharedAssigns demo

--- test/shared_assigns_demo_web/controllers/page_controller_test.exs
+++ test/shared_assigns_demo_web/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule SharedAssignsDemoWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get(conn, ~p"/")
-    assert html_response(conn, 200) =~ "Peace of mind from prototype to production"
+    assert html_response(conn, 200) =~ "SharedAssigns Demo"
   end
 end
