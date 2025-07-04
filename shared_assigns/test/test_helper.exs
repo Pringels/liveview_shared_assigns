39312b762cc322d934e31c55ef4e865638e76d6e
Fly Dev <dev@fly.io>
2025-07-04 12:59:51 +0000

    add test helper with minimal endpoint

--- shared_assigns/test/test_helper.exs
+++ shared_assigns/test/test_helper.exs
@@ -2,3 +2,26 @@ ExUnit.start()
 
 # Configure ExUnit for async testing
 ExUnit.configure(exclude: [:skip], formatters: [ExUnit.CLIFormatter])
+
+# Define a minimal endpoint for testing
+defmodule SharedAssigns.TestEndpoint do
+  use Phoenix.Endpoint, otp_app: :shared_assigns
+
+  @session_options [
+    store: :cookie,
+    key: "_shared_assigns_test",
+    signing_salt: "test_salt"
+  ]
+
+  plug(Plug.Session, @session_options)
+  plug(Phoenix.LiveView.Flash)
+
+  def config(which)
+  def config(:live_view), do: [signing_salt: "test_salt"]
+  def config(_), do: nil
+
+  defoverridable config: 1
+end
+
+# Start the test endpoint
+{:ok, _} = SharedAssigns.TestEndpoint.start_link()
